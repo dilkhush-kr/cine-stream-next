8b86b07923eb6aeecf669740196199dd8762cdf4
Dilkhush Kumar <dilkhushmehus@gmail.com>
2026-03-13 19:40:13 +0530

    delete comment

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,3 @@
-// "use client";
-
-// import { useState } from "react";
-
-// export default function SearchBar({ onSearch }) {
-
-//   const [query,setQuery] = useState("");
-
-//   const handleSearch = (e) => {
-
-//     const value = e.target.value;
-//     setQuery(value);
-
-//     onSearch(value);
-//   };
-
-//   return(
-
-//     <input
-//       className="search-bar"
-//       type="text"
-//       placeholder="Search movies..."
-//       value={query}
-//       onChange={handleSearch}
-//     />
-
-//   )
-// }
-
 
 "use client";
 
